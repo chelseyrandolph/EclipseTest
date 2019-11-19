3622f25d95b5ae0a7f41d48e3e6964642da8735c
Chelsey Randolph <chelseyrrandolph@gmail.com>
2019-11-19 12:52:58 -0500

    Commit #2: Goodbye World

---
+++ src/com/chelseycodes/example/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package com.chelseycodes.example;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye World!");
+	}
+
+}
